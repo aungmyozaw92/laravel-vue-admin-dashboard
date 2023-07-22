497adf0109cf9244b90051ef5dcdb4abd11fa64e
marathonamz <aungmyozaw@marathonmyanmar.com>
2023-07-22 11:12:11 +0630

    setup vue router

---
+++ resources/js/routes.js
@@ -0,0 +1,9 @@
+import Dashboard from "./components/admin/Dashboard.vue";
+
+export default [
+    {
+        path: "/admin/dashboard",
+        name: "admin.dashboard",
+        component: Dashboard,
+    },
+];
